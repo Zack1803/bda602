914962d447aaedcafc2f9c98a30f001c30100ed5
zack <mpatil6564@sdsu.edu>
2022-09-23 16:30:59 -0700

    after PR

--- hw2/hw2.sql
+++ hw2/hw2.sql
@@ -31,62 +31,27 @@ group by a.batter;
 
 #Question 3
 
-#Q3 - v1
-WITH batters as
-    (
-select  g.local_date ,a.batter, a.Hit,a.atBat
-from
-batter_counts as A
-JOIN game g on A.game_id = g.game_id
-group by a.batter)
-
-select date(local_date) as Date,
-       batter,
-       AVG((Hit/atBat)*100) OVER(ORDER BY local_date ROWS BETWEEN 99 PRECEDING AND CURRENT ROW)
-        AS moving_average_100_days
-from batters;
-
-
-#Q3 - v2
-
-WITH batter as
-(select  cast(g.local_date as date) as date ,a.batter, a.Hit,a.atBat
-from
-batter_counts as A
-JOIN game g
-on A.game_id = g.game_id
-where g.local_date >= Date_SUB((select max(local_date) from game),interval 100 day )
-group by a.batter)
-
-select distinct batter, (Hit)/(atBat)
-from batter
-group by  batter;
-
-
-
-
-
-
 #Q3 - Probably Correct
 
 #Step 1 - Create a temp table with all required columns
 
 #drop table before creation
 
-create or replace temporary table rolling_average_intermediate as
-SELECT a.batter, b.local_date, A.hit,A.atBat
+create or replace table rolling_average
+with rolling_average_intermediate as
+(SELECT a.batter, b.local_date, A.hit,A.atBat
 FROM batter_counts as A
 LEFT JOIN GAME as B
-ON A.game_id = B.game_id ;
+ON A.game_id = B.game_id)
 
 
 #Step 2 - Use the Temp Table to create self join and find rolling average
-#Hint 2 - for one batter - commented for submission
-create or replace table rolling_average
-select a.batter, a.local_date, (sum(a.Hit)/NULLIF(sum(a.atBat),0))*100 as rolling_avg
+#Hint 2 - for one batter
+
+select a.batter, a.local_date, (sum(b.Hit)/NULLIF(sum(b.atBat),0))*100 as rolling_avg
 from rolling_average_intermediate as a
 join rolling_average_intermediate as b
-on a.batter = b.batter and a.local_date > b.local_date and b.local_date between a.local_date - INTERVAL 100 DAY and a.local_date
-#where a.batter = 451088
+on a.batter = b.batter and a.local_date > b.local_date and b.local_date between  a.local_date - INTERVAL 100 DAY and a.local_date
+where a.batter = 110029
 group by a.batter,a.local_date
 order by a.local_date DESC;
