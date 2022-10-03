cef49ea0d693af3ca299f2c3b314b12e65555878
zack <mpatil6564@sdsu.edu>
2022-10-02 23:01:19 -0700

    updated repo with hw2

---
+++ hw2/hw2.sql
@@ -0,0 +1,56 @@
+use baseball;
+# Question 1 -  Annual
+
+#create a new table
+
+create or replace table annual_average
+select DISTINCT a.batter,extract (year from g.local_date) as year ,(sum(a.Hit)/NULLIF(sum(a.atBat),0))*100 as batting_average
+from
+batter_counts as A
+JOIN game g on A.game_id = g.game_id
+group by a.batter, year;
+
+
+
+#Question 2 - Historical
+
+
+#create a new table
+
+create or replace table historical_average
+select DISTINCT a.batter,(sum(a.Hit)/NULLIF(sum(a.atBat),0))*100 as batting_average
+from
+batter_counts as A
+JOIN game g on A.game_id = g.game_id
+group by a.batter;
+
+#Question 3
+
+#Q3 - Probably Correct
+
+#Step 1 - Create a temp table with all required columns
+
+#drop table before creation
+
+#create or replace table rolling_average
+create or replace temporary table rolling_average_intermediate
+(SELECT a.batter, b.local_date, A.hit,A.atBat
+FROM batter_counts as A
+LEFT JOIN GAME as B
+ON A.game_id = B.game_id);
+CREATE INDEX batter ON rolling_average_intermediate(batter);
+CREATE  INDEX date ON rolling_average_intermediate(local_date);
+CREATE  INDEX batter_date ON rolling_average_intermediate(batter,local_date);
+
+
+
+#Step 2 - Use the Temp Table to create self join and find rolling average
+#Hint 2 - for one batter
+#create or replace table rolling_average
+select a.batter, a.local_date, (sum(b.Hit)/NULLIF(sum(b.atBat),0)) as rolling_avg
+from rolling_average_intermediate as a
+join rolling_average_intermediate as b
+on a.batter = b.batter and a.local_date > b.local_date and b.local_date between  a.local_date - INTERVAL 100 DAY and a.local_date
+#on a.batter = b.batter and a.local_date > DATE_SUB(b.local_date, INTERVAL 100 DAY)
+group by a.batter,a.local_date
+order by a.local_date DESC;
